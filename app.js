c75cbdcfc17a7dc36597273655a92198e2722139
Hayden Edward Davies <hbgamer5@gmail.com>
2015-02-13 07:01:03 -0500

    Added censoring and HTML injection

--- app.js
+++ app.js
@@ -4,19 +4,29 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var sanitizeHtml = require('sanitize-html');
 
+var substrings = ["fuck", "shit", "piss", "patrick", "cunt", "bastard"];
+
 function cleanMessage(message) {
-	message = message.replace(/fuck/gi, "****");
-	message = message.replace(/shit/gi, "****");
+	message = message.replace("<", "&lt;");
+	if (substrings.some(function(v) { 
+		var repl = v;
+		message = message.replace(new RegExp(v, "gi"), new Array(v.length + 1).join( "*" ));
+	}));
 	return message;
 }
 
 io.on('connection', function(client) {
     client.on('join', function(name) {
-        client.nickname = name;
+        client.nickname = cleanMessage(name);
     });
     client.on('messages', function(message) {
+	message = message + " ";
         var nickname = client.nickname;
-	switch (message) {
+	switch (message.substr(0, message.indexOf(" "))) {
+		case "/shout":
+			client.broadcast.emit('messages', "<h1>"+message.replace("/shout", "")+"</h1>");
+			client.emit('messages', "<h1>"+message.replace("/shout", "")+"</h1>");
+			break;
 		case "/panda":
 			client.broadcast.emit('messages', nickname + ": <img src='panda.jpg'/>");
 			client.emit('messages', nickname + ": <img src='panda.jpg' />");
@@ -26,11 +36,7 @@ io.on('connection', function(client) {
 			client.emit('clear', "c");		
 			break;
 		default:
-			clean = sanitizeHtml(message, {
-				allowedTags: [ 'b', 'i', 'em', 'strong'],
-				allowedAttributes: {}
-			});
-			clean = cleanMessage(clean);
+			var clean = cleanMessage(message);
 			client.broadcast.emit('messages', nickname + ": " + clean);
 			client.emit('messages', nickname + ": " + clean);
 	}
